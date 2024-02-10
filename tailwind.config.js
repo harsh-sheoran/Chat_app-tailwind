7809575b443ff9e1631df85978922426ca88171d
harsh-sheoran_399 <harshsheoran9350@gmail.com>
2024-02-09 23:32:54 -1200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
